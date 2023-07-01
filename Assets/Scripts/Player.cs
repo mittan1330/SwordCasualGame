f90de741046759dbedaeedfda16abdaa2dfa1aef
mittan1330 <64365341+mittan1330@users.noreply.github.com>
2023-07-01 11:46:09 +0900

    add: アニメーションを追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class layer : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField]
     float playerSpeed = 10.0f;
