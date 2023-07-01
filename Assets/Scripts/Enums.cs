4b5518e64c3cb1d82f346b67613137f389e136a8
mittan1330 <64365341+mittan1330@users.noreply.github.com>
2023-07-01 11:56:22 +0900

    add:アニメーション制御機能を追加

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,7 @@
+public enum CharactorState
+{
+    Run,
+    Attack,
+    Dead,
+    Damage,
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,28 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     float playerSpeed = 10.0f;
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    CharactorState charactorState;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        charactorState = CharactorState.Run;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (charactorState)
+        {
+            case CharactorState.Run:
+                animator.SetBool("Run", true);
+                break;
+
+        }
         transform.Translate(0, 0, playerSpeed * Time.deltaTime);
     }
 }
